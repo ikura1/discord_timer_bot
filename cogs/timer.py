196f629107cde9b4f321d608de4e3073eb2c8809
ikura1 <ikura.lab@gmail.com>
2022-01-25 15:56:35 +0900

    feat: ユーザーがVCにいる場合接続し音をながす

--- cogs/timer.py
+++ cogs/timer.py
@@ -1,7 +1,13 @@
 import asyncio
+from pathlib import Path
 
 from discord.ext import commands
+from discord.channel import VoiceChannel
+from discord import FFmpegPCMAudio
 import emoji
+
+DIRECTORY = Path(__file__).parent.parent
+FFMPEG = DIRECTORY / "ffmpeg" / "bin" / "ffmpeg.exe"
 
 
 class Timer(commands.Cog):
@@ -15,9 +21,30 @@ class Timer(commands.Cog):
     async def timer(self, ctx, time: str = None):
         await asyncio.sleep(int(time))
         # TODO: 経過時間の表示したいなー
-        user = ctx.message.author
         # TODO: ボイスチャットにユーザーがいるなら音声で通知したい
-        message = await ctx.send(f"{user.mention} {time}秒が経過しました")
+        user = ctx.message.author
+        if user.voice:
+            await self.voice_notification(ctx, time)
+        await self.text_notification(ctx, time)
+
+    async def voice_notification(self, ctx, time):
+        user = ctx.message.author
+        # 接続
+        voiceChannel = await VoiceChannel.connect(user.voice.channel)
+        # 再生
+        path = DIRECTORY / "clock.mp3"
+        voiceChannel.play(
+            FFmpegPCMAudio(str(path), executable=str(FFMPEG)),
+        )
+        # 切断
+        # もっと時間置いて切断でも良い
+        while voiceChannel.is_playing():
+            await asyncio.sleep(1)
+        await voiceChannel.disconnect()
+
+    async def text_notification(self, ctx, time):
+        user = ctx.message.author
+        message = await ctx.send(f"{user.mention} {time}秒タイマーが終了しました")
         check = emoji.emojize(":check_mark:", use_aliases=True)
         x = emoji.emojize(":x:", use_aliases=True)
         await message.add_reaction(check)
